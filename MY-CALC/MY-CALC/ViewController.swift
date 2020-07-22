1f501e95f54ee8994c1b6263aa8a719585eb046d
jialihan <jellyhan27@gmail.com>
2020-07-22 01:16:26 -0700

    fix: add calculator project

---
+++ MY-CALC/MY-CALC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MY-CALC
+//
+//  Created by Han Jiali on 7/22/20.
+//  Copyright © 2020 Han Jiali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ### 1. layout with stack view
 ### 2. responsive UI design in landscape and portrait
 ### 3. functionality will come soon ~~~
+
+<img src="cover1.png" width="171" height="370.5" >
+<img src="cover2.png" width="365" height="176" >
